4978177dde1c7110bb7dd0ac06bf0165ec97d734
Simon Haddad <simon.haddad.41430@gmail.com>
2024-08-31 18:16:09 +0900

    Update AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "com.shio.saikyo"
-        minSdk = 30
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
